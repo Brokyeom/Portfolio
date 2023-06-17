4dc55d746ca8a61da59f693823bbf247ff15794e
Brokyeom <goindol40@gmail.com>
2023-06-17 21:02:20 +0900

    feat : 레이아웃 구성

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+</script>
+
+<header class="sticky flex flex-row justify-between items-center w-full h-24 px-48">
+	<h1>김형겸</h1>
+	<nav class='flex flex-row gap-8'><a href="/">Introduce</a><a href="/portfolio">Portfolio</a><a href="activities">Activities</a></nav>
+</header>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.800);
+		color: white;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>안녕하세요<br />웹 FE 개발자 김형겸입니다!</h1>
