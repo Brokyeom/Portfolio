0265de195cb20db078cb932d171e038b568e8066
Brokyeom <goindol40@gmail.com>
2023-06-18 01:36:06 +0900

    refactor : Typescript 적용

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,51 @@
+<script lang="ts">
+	type SectionContent = {
+	  text: string;
+	  link?: string;
+	};
+  
+	type Section = {
+	  title: string;
+	  content: SectionContent[];
+	};
+  
+	let sections: Section[] = [
+	  {
+		title: 'Contact',
+		content: [
+		  { text: 'Phone : 010 - 5787 - 1603' },
+		  { text: 'Email : goindol40@gmail.com' },
+		],
+	  },
+	  {
+		title: 'Link',
+		content: [
+		  { text: 'Github', link: 'https://github.com/Brokyeom' },
+		  { text: 'Velog', link: 'https://velog.io/@brokyeom' },
+		  { text: 'HuggingFace', link: 'https://huggingface.co/Brokyeom' },
+		],
+	  },
+	];
+  </script>
+
 <main>
 	<h1 class="text-6xl font-extrabold leading-relaxed mb-20">안녕하세요,<br />웹 FE 개발자 김형겸입니다!</h1>
 
 	<article class="flex gap-20">
-	<section>
-		<h2>Contact</h2>
-		<p>Phone : 010 - 5787 - 1603</p>
-		<p>Email : goindol40@gmail.com</p>
-	</section>
-	<section>
-		<h2>Link</h2>
-		<p><a href='https://github.com/Brokyeom'>Github</a></p>
-		<p><a href='https://velog.io/@brokyeom'>Velog</a></p>
-		<p><a href='https://huggingface.co/Brokyeom'>HuggingFace</a></p>
-	</section>
+		{#each sections as { title, content }}
+		<section>
+		  <h2>{title}</h2>
+		  {#each content as { text, link }}
+			<p>
+			  {#if link}
+				<a href={link}>{text}</a>
+			  {:else}
+				{text}
+			  {/if}
+			</p>
+		  {/each}
+		</section>
+	  {/each}
 	</article>
 </main>
 
