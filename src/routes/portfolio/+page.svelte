5b2c69dd10dde0c58d7ef40c8bef8d9e396e4644
Brokyeom <goindol40@gmail.com>
2023-06-19 19:54:41 +0900

    feat : 포트폴리오 nav 퍼블리싱

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,1 +1,26 @@
+<script lang="ts">
+    let portfolios: string[] = ['SOPT Web Admin', 'Pic.me', 'nori'];
+    let currentProject: string = portfolios[0];
+
+    const updateCurrentProject = (name: string) => {
+        if ((document as any).startViewTransition) {
+            (document as any).startViewTransition(() => {
+                currentProject = name;
+                return Promise.resolve();
+            });
+        } else {
+            currentProject = name;
+        }
+    };
+</script>
+
 <h2 class="text-5xl">포트폴리오</h2>
+
+<article>
+    <nav class="flex flex-row gap-10 mb-10">
+    {#each portfolios as name (name)}
+        <p class='text-3xl cursor-pointer hover:text-red-300' on:click={() => updateCurrentProject(name)}>{name}</p>
+    {/each}
+    </nav>
+    <h3 class="text-4xl font-semibold">{currentProject}</h3>
+</article>
