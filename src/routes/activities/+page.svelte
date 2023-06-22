f5126c87e4df9ed07b5d39964985c11d62f747ca
Brokyeom <goindol40@gmail.com>
2023-06-23 00:07:17 +0900

    chore : Add desc property

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -2,9 +2,15 @@
 	let activities = [
 		{
 			year: 2023,
-			detail: [{ title: 'SOPT Makers 2기' }],
-	    },
-		{ year: 2022, detail: [{ title: 'SOPT 31기 WEB파트  OB' }, { title: 'SOPT 30기 WEB파트 YB' }] },
+			detail: [{ title: 'SOPT Makers 2기', desc: '운영서비스 팀 FE Developer' }],
+		},
+		{
+			year: 2022,
+			detail: [
+				{ title: 'SOPT 31기 WEB파트  OB', desc: '' },
+				{ title: 'SOPT 30기 WEB파트 YB', desc: '' },
+			],
+		},
 	];
 </script>
 
